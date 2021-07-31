9ebee3c48e06d7322f751b01b5c74dc198dae92f
vybrid <83131413+vybrid@users.noreply.github.com>
2021-07-31 21:39:12 +0530

    Create LoginFailure.java

---
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/LoginFailure.java
@@ -0,0 +1,26 @@
+package com.example.cs2ex1st;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginFailure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_failure);
+    }
+
+    public void tryAgain(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
+
+    public void signUpInstead(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
