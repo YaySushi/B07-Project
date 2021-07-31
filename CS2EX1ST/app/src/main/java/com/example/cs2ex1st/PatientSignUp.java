d482cb783358d843171b1c0878f4a1a6b3df3231
vybrid <83131413+vybrid@users.noreply.github.com>
2021-08-01 00:01:57 +0530

    Update PatientSignUp.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/PatientSignUp.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/PatientSignUp.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class PatientSignUp extends AppCompatActivity {
+public class PatientSignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_sign_up);
+        Spinner spinner = (Spinner) findViewById(R.id.patientGenderSpinner);
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(genderAdapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void submitPatientData(View view) {
@@ -20,4 +29,14 @@ public class PatientSignUp extends AppCompatActivity {
         Intent intent = new Intent(this, SignUpSuccess.class);
         startActivity(intent);
     }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        gender = (String)parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        gender = "Male";
+    }
+
 }
