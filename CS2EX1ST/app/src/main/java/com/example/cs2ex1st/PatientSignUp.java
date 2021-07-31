9d32e5f83fe1abee195bc1e9334928553d62af26
vybrid <83131413+vybrid@users.noreply.github.com>
2021-07-31 21:39:17 +0530

    Update PatientSignUp.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/PatientSignUp.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/PatientSignUp.java
@@ -2,7 +2,9 @@ package com.example.cs2ex1st;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PatientSignUp extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PatientSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_sign_up);
+    }
+
+    public void submitPatientData(View view) {
+        // need 2 cases: failure and success
+        // ***INCOMPLETE***
+        Intent intent = new Intent(this, SignUpSuccess.class);
+        startActivity(intent);
     }
 }
