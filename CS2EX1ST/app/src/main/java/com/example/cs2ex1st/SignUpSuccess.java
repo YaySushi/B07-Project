18b9353361868295fbea132d36f4b07b640ac24c
vybrid <83131413+vybrid@users.noreply.github.com>
2021-07-31 21:39:20 +0530

    Create SignUpSuccess.java

---
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/SignUpSuccess.java
@@ -0,0 +1,21 @@
+package com.example.cs2ex1st;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_success);
+    }
+
+    public void goToLogIn(View view) {
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+    }
+}
