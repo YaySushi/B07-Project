183be5ebf3848a80b643c888db8c6ce7ca0f8cf6
vybrid <83131413+vybrid@users.noreply.github.com>
2021-08-01 18:48:59 +0530

    Update SignUpFailure.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/SignUpFailure.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/SignUpFailure.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SignUpFailure extends AppCompatActivity {
 
@@ -12,6 +13,9 @@ public class SignUpFailure extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_failure);
+
+        TextView textView = findViewById(R.id.errorMessage);
+        textView.setText(getIntent().getStringExtra("ERROR_MESSAGE"));
     }
 
     public void trySignUpAgain(View view) {
