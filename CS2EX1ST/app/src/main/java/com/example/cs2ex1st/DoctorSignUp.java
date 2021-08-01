de64ec1cb5df4b90eb1a5978d9d1921d92a595a7
vybrid <83131413+vybrid@users.noreply.github.com>
2021-08-01 16:57:18 +0530

    Update DoctorSignUp.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/DoctorSignUp.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/DoctorSignUp.java
@@ -9,33 +9,35 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class DoctorSignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class DoctorSignUp extends AppCompatActivity {
 
-    private String gender;
+    private Doctor d;
+    private Spinner spinner1;
+    private Spinner spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_sign_up);
-        Spinner spinner = (Spinner) findViewById(R.id.doctorGenderSpinner);
-        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(genderAdapter);
-        spinner.setOnItemSelectedListener(this);
+
+        spinner1 = (Spinner) findViewById(R.id.doctorGenderSpinner);
+        ArrayAdapter<CharSequence> genderAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.gender));
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(genderAdapter);
+
+        spinner2 = (Spinner) findViewById(R.id.doctorSpecialization);
+        ArrayAdapter<CharSequence> specializationAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.specialization));
+        specializationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(specializationAdapter);
     }
 
     public void submitDoctorData(View view) {
+        User d = new Doctor(spinner1.getSelectedItem(), spinner2.getSelectedItem());
         // need 2 cases: failure and success
         // ***INCOMPLETE***
         Intent intent = new Intent(this, SignUpSuccess.class);
         startActivity(intent);
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        gender = (String)parent.getItemAtPosition(pos);
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        gender = "Male";
     }
 }
