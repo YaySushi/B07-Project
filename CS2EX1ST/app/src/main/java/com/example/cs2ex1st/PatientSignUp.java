b4e29737cd0f7a9d1182abfff417505c7b4ca213
vybrid <83131413+vybrid@users.noreply.github.com>
2021-08-01 16:57:20 +0530

    Update PatientSignUp.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/PatientSignUp.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/PatientSignUp.java
@@ -11,32 +11,30 @@ import android.widget.Spinner;
 
 public class PatientSignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private String gender;
+    private Patient p;
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_sign_up);
-        Spinner spinner = (Spinner) findViewById(R.id.patientGenderSpinner);
-        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(genderAdapter);
-        spinner.setOnItemSelectedListener(this);
+
+        spinner1 = (Spinner) findViewById(R.id.doctorGenderSpinner);
+        ArrayAdapter<CharSequence> genderAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.gender));
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(genderAdapter);
     }
 
     public void submitPatientData(View view) {
+        User p = new Patient(spinner1.getSelectedItem());
+        // add try-catch blocks
+
+        // then search database
         // need 2 cases: failure and success
         // ***INCOMPLETE***
         Intent intent = new Intent(this, SignUpSuccess.class);
         startActivity(intent);
-    }
-
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        gender = (String)parent.getItemAtPosition(pos);
-    }
-
-    public void onNothingSelected(AdapterView<?> parent) {
-        gender = "Male";
     }
 
 }
