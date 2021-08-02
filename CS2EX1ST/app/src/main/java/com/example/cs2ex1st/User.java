0e08e7afd4cc29584188d6e10897643c051eeb31
vybrid <83131413+vybrid@users.noreply.github.com>
2021-08-02 22:31:57 +0530

    Update User.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/User.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/User.java
@@ -19,7 +19,7 @@ public abstract class User {
             throw new InputMismatchException("Invalid name.");
         }
 
-        if (Pattern.matches("[a-zA-Z0-9]@[a-zA-Z0-9]+.[a-zA-Z0-9]+|[a-zA-Z0-9]+[a-zA-Z0-9\\.]*[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+", email)) {
+        if (!Pattern.matches("[a-zA-Z0-9]@[a-zA-Z0-9]+.[a-zA-Z0-9]+|[a-zA-Z0-9]+[a-zA-Z0-9\\.]*[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+", email)) {
             throw new InputMismatchException("Invalid email.");
         }
 
