031364ebbfa68ddb168cc63ac332b8f4f41399a4
Amy Yao <AmyXi.Yao@gmail.com>
2021-08-03 12:14:55 -0400

    Created doctor past appointment activity

---
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/ApptAdapter.java
@@ -0,0 +1,64 @@
+package com.example.cs2ex1st;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ApptAdapter extends RecyclerView.Adapter<ApptAdapter.ViewHolder> {
+    // List of appointments
+    private ArrayList<Appointment> appointments;
+
+    // Custom ViewHolder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView doctorNameTextView;
+        private TextView apptTimeTextView;
+
+        public ViewHolder(View view) {
+            super(view);
+
+            doctorNameTextView = (TextView)view.findViewById(R.id.doctor_name);
+            apptTimeTextView = (TextView)view.findViewById(R.id.appt_time);
+        }
+    }
+
+    // Constructor
+    public ApptAdapter(ArrayList<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
+    // Adapter methods that need to be overridden
+    // Creates new views
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.item_appt, viewGroup, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    // Put content into views
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        // Get Appointment at position
+        Appointment cur = appointments.get(position);
+
+        // Set the views based on cur data
+        TextView docName = viewHolder.doctorNameTextView;
+        docName.setText(cur.getDoctor().getFirstName() + " " + cur.getDoctor().getLastName());
+
+        TextView apptTime = viewHolder.apptTimeTextView;
+        apptTime.setText("Time: " + cur.getStartTime());
+    }
+
+    // Return item count
+    @Override
+    public int getItemCount() {
+        return appointments.size();
+    }
+}
---
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/DoctorPastAppt.java
@@ -0,0 +1,74 @@
+package com.example.cs2ex1st;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DoctorPastAppt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_past_appt);
+
+        RecyclerView rvDocPastAppt = (RecyclerView)findViewById(R.id.recyclerPastAppt);
+
+        // Get Doctor's list of appointments
+
+        // Create adapter and pass in appointment data
+
+        // Attach adapter to RecyclerView
+
+        // Set Layout manager
+
+
+
+        /*
+
+        // TESTING data
+        ArrayList<Appointment> appointments = new ArrayList<>();
+
+        Doctor doc1 = new Doctor("first", "last", "a@b.c", "F", "spec", "pass");
+        Appointment app1 = new Appointment(11, true);
+        app1.setDoctor(doc1);
+
+        Appointment app2 = new Appointment(12, true);
+        app2.setDoctor(doc1);
+
+        Appointment app3 = new Appointment(13, true);
+        app3.setDoctor(doc1);
+
+        Appointment app4 = new Appointment(14, true);
+        app4.setDoctor(doc1);
+
+        Appointment app5 = new Appointment(15, true);
+        app5.setDoctor(doc1);
+
+        Appointment app6 = new Appointment(16, true);
+        app6.setDoctor(doc1);
+
+        appointments.add(app1);
+        appointments.add(app2);
+        appointments.add(app3);
+        appointments.add(app4);
+        appointments.add(app5);
+        appointments.add(app6);
+
+        // Create adapter and pass in appointment data
+        ApptAdapter adapter = new ApptAdapter(appointments);
+
+        // Attach adapter to RecyclerView
+        rvDocPastAppt.setAdapter(adapter);
+
+        // Set Layout manager
+        rvDocPastAppt.setLayoutManager(new LinearLayoutManager(this));
+
+        */
+
+    }
+}
