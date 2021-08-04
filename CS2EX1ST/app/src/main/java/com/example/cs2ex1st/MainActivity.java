b0ccf492aa73950d2e04bbb35f2e7b496eed2918
YaySushi <ducklingdood@gmail.com>
2021-08-04 15:50:24 -0400

    example of writing to database in MainActivity.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/MainActivity.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.firebase.database.DatabaseReference;
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User d1;
+        String email = "asdfasdf@gmail.com";
+        d1 = new Doctor("asdf", "asdf", email, "asdfasdf?", "asdfasdf", "passsssssss");
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("sushi writing this").child("sushi's child").setValue(d1);
+        Log.i("TESTINGGGGG", "HI HELLO THERE"); //check in logcat.
     }
 
     public void sendSignUpMessage(View view) {
