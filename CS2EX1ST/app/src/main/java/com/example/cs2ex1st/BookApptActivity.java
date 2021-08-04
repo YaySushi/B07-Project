273143dab3053997494a8af91e373eb6030a826f
ectodrop <40843847+ectodrop@users.noreply.github.com>
2021-08-04 02:04:13 -0400

    .

---
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/BookApptActivity.java
@@ -0,0 +1,16 @@
+package com.example.cs2ex1st;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookApptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_appt);
+    }
+
+
+}
