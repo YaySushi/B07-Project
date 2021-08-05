a8ac343433ee7bcc6d529ec017a22a46933118a9
vybrid <83131413+vybrid@users.noreply.github.com>
2021-08-05 13:00:13 +0530

    Update MainActivity.java

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/MainActivity.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.cs2ex1st;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
