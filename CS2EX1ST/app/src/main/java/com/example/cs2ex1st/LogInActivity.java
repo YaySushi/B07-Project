5093615942ebcad430d500466d47161cde62de87
Amy Yao <AmyXi.Yao@gmail.com>
2021-08-09 18:24:16 -0400

    Refactored LogInActivity to conform to MVP

--- CS2EX1ST/app/src/main/java/com/example/cs2ex1st/LogInActivity.java
+++ CS2EX1ST/app/src/main/java/com/example/cs2ex1st/LogInActivity.java
@@ -16,54 +16,44 @@ import com.google.firebase.database.ValueEventListener;
 
 public class LogInActivity extends AppCompatActivity {
     private TextView logInErrorText;
-    private User u;
+    private Presenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         logInErrorText = (TextView) findViewById(R.id.errorText1);
         logInErrorText.setText("");
+
+        presenter = new Presenter(new Model(), this);
+    }
+
+    public void displayMessage(String message) {
+        logInErrorText.setText(message);
+    }
+
+    public String getEmail() {
+        EditText editText = (EditText) findViewById(R.id.emailaddress);
+        return editText.getText().toString();
+    }
+
+    public String getPassword() {
+        EditText editText = (EditText) findViewById(R.id.password);
+        return editText.getText().toString();
     }
 
     public void sendLogInInfo(View view) {
-        EditText editText = (EditText) findViewById(R.id.emailaddress);
-        String email = editText.getText().toString();
-        email = email.replace('.', '*');
-        editText = (EditText) findViewById(R.id.password);
-        String password = editText.getText().toString();
-        if (email.equals("")) {
-            logInErrorText.setText("Email address cannot be empty.");
-            return;
-        }
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("ID/" + email);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    String[] info = ((String)dataSnapshot.getValue()).split(", ");
-                    if (info[0].equals(password)) {
-                        if (info[1].equals("Doctor")) {
-                            LoggedInUser.setUser((String)dataSnapshot.getKey(), "Doctor");
-                            Intent intent = new Intent(LogInActivity.this, DoctorProfile.class);
-                            startActivity(intent);
-                        } else {
-                            LoggedInUser.setUser((String)dataSnapshot.getKey(), "Patient");
-                            Intent intent = new Intent(LogInActivity.this, PatientProfile.class);
-                            startActivity(intent);
-                        }
-                    } else {
-                        logInErrorText.setText("Password is incorrect.");
-                        return;
-                    }
-                } else {
-                    logInErrorText.setText("Email address is incorrect.");
-                    return;
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.toException());
-            }
-        });
+        presenter.validateUser();
     }
+
+    public void startDoctorProfile() {
+        Intent intent = new Intent(LogInActivity.this, DoctorProfile.class);
+        startActivity(intent);
+    }
+
+    public void startPatientProfile() {
+        Intent intent = new Intent(LogInActivity.this, PatientProfile.class);
+        startActivity(intent);
+    }
+
 }
